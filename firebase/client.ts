38afab77a7cc30ed97c97184123feb0b40c8572e
suresh k <binaryshade@binaryshade.binaryshade>
2025-04-05 11:38:59 +0530

    init commit

--- firebase/client.ts
+++ firebase/client.ts
@@ -2,13 +2,13 @@ import { initializeApp, getApp, getApps } from "firebase/app";
 import {getFirestore} from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 const firebaseConfig = {
-  apiKey: "AIzaSyAjHOHlfeQ43kBcRz0p-dVasAWzWw6Ujr4",
-  authDomain: "ai-mocker-a1f07.firebaseapp.com",
-  projectId: "ai-mocker-a1f07",
-  storageBucket: "ai-mocker-a1f07.firebasestorage.app",
-  messagingSenderId: "968773995054",
-  appId: "1:968773995054:web:bd91ad442c3589d16a6dbf",
-  measurementId: "G-YLZ12KBF52"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
